93e7f7736d6a924d682a3af99af65d3d3dc03669
Adrián Ferrando <adri.vlc91@gmail.com>
2024-08-12 18:10:58 +0200

    light helper

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -2,33 +2,49 @@
 
 import { Canvas } from "@react-three/fiber";
 import { ProjectOne } from "./projects/ProjectOne";
+import * as THREE from "three";
 import {
   AccumulativeShadows,
   Environment,
   Loader,
   OrbitControls,
   RandomizedLight,
+  useHelper,
 } from "@react-three/drei";
-import { useState, useTransition } from "react";
+import { Suspense, useRef, useState, useTransition } from "react";
 import { useControls } from "leva";
+import { DirectionalLight, DirectionalLightHelper } from "three";
 
 export default function Drawer() {
   return (
+    <Canvas shadows camera={{ position: [0, 0, 20] }}>
+      <Lights />
+      <ProjectOne position={[0, -12, 0]} />
+      <OrbitControls
+        // autoRotate
+        autoRotateSpeed={2}
+        enablePan={false}
+        //enableZoom={false}
+        minPolarAngle={Math.PI / 2}
+        maxPolarAngle={Math.PI / 2}
+      />
+    </Canvas>
+  );
+}
+
+function Lights() {
+  const dirLight = useRef<DirectionalLight>(null as any);
+  useHelper(dirLight, DirectionalLightHelper, 1, "red");
+
+  return (
     <>
-      <Canvas shadows camera={{ position: [0, 0, 50], fov: 40 }}>
-        <ProjectOne position={[0, -12, 0]} />
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[10, 10, 5]} intensity={1} castShadow />;
-        <OrbitControls
-          autoRotate
-          autoRotateSpeed={2}
-          enablePan={false}
-          enableZoom={false}
-          minPolarAngle={Math.PI / 2}
-          maxPolarAngle={Math.PI / 2}
-        />
-      </Canvas>
-      <Loader />
+      <ambientLight intensity={0.4} />
+      <directionalLight
+        ref={dirLight}
+        position={[10, 15, 30]}
+        intensity={1.5}
+        castShadow
+      />
     </>
   );
 }
